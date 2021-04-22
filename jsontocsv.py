68f0f860a4ca1d26d7b5d5616721095c62489bc0
Hitesh Mishra <hiteshmishra708@gmail.com>
2021-04-22 22:34:06 +0530

    new changes

--- jsontocsv.py
+++ jsontocsv.py
@@ -1,23 +1,15 @@
-import csv
+import csv, json
 
 def convert_json_to_csv(file):
-    import json
-    # Opening JSON file
     f = open(file + '.json',)
     
-    # returns JSON object as 
-    # a dictionary
-    data = json.load(f)
-    
-    # Iterating through the json
-    # list
-    print(data)
-    for i in data['emp_details']:
-        print(i)
-    
-    # Closing file
+    records = json.load(f)
+    with open(file + '.csv', mode='w') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=',')
+        csv_writer.writerow(['Entity Name', 'Patterns', 'Response'])
+        for record in records:
+            csv_writer.writerow(['', record['question'].encode('utf-8').strip(), record['answer'].encode('utf-8').strip()])
     f.close()
-    pass
 
 convert_json_to_csv('java')
 convert_json_to_csv('python')
